1fff1520fff2fd42472c240f54c12fd679024bde
Dasdoria1O1 <73320019+Dasdoria1O1@users.noreply.github.com>
2025-10-03 10:39:42 -0500

    Update ProfileUIState.kt
    cambio

--- app/src/main/java/com/example/ratingroom/ui/screens/profile/ProfileUIState.kt
+++ app/src/main/java/com/example/ratingroom/ui/screens/profile/ProfileUIState.kt
@@ -1,5 +1,7 @@
 package com.example.ratingroom.ui.screens.profile
 
+import com.example.ratingroom.data.models.Review
+
 data class ProfileData(
     val name: String,
     val email: String,
@@ -14,5 +16,8 @@ data class ProfileUIState(
     val profileData: ProfileData? = null,
     val isDarkMode: Boolean = false,
     val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
+    val userReviews: List<Review> = emptyList(),
+    val reviewsCount: Int = 0,
+    val averageRating: Double = 0.0
 )
