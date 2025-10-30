f2da568787025c60e0a6bec2f66d4c25910aa58f
DavidSP12 <dpineros05@gmail.com>
2025-10-29 20:51:34 -0500

    Update emulator settings
    - Modified BaseApplication.kt to introduce a new boolean flag for emulator usage, allowing for conditional emulator initialization based on the flag's value.

--- app/src/main/java/com/example/ratingroom/BaseApplication.kt
+++ app/src/main/java/com/example/ratingroom/BaseApplication.kt
@@ -1,5 +1,6 @@
 package com.example.ratingroom
 
+import android.R
 import android.app.Application
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
@@ -9,10 +10,13 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class BaseApplication: Application() {
     override fun onCreate() {
+        val Pruebas: Boolean = false
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Firebase.firestore.useEmulator("10.0.2.2", 8080)
-            Firebase.auth.useEmulator("10.0.2.2", 9099)
+        if(Pruebas == true){
+            if (BuildConfig.DEBUG) {
+                Firebase.firestore.useEmulator("10.0.2.2", 8080)
+                Firebase.auth.useEmulator("10.0.2.2", 9099)
+            }
         }
     }
 }
